ab42a6e3f487748e83dd3b1bac34abaade044018
mattmiller03 <44274118+mattmiller03@users.noreply.github.com>
2025-08-08 08:18:22 -0400

    adding esxi model

---
+++ Models/EsxiHost.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace VCenterMigrationTool.Models;
+
+public partial class EsxiHost : ObservableObject
+{
+    public string Name { get; set; } = string.Empty;
+    public string Cluster { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public bool IsSelected { get; set; }
+}
