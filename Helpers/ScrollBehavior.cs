6fd97f27ee619a35dda2890b0a4ed87a8f8256a5
mattmiller03 <44274118+mattmiller03@users.noreply.github.com>
2025-08-16 12:56:59 -0400

    UI fixed

--- Helpers/ScrollBehavior.cs
+++ Helpers/ScrollBehavior.cs
@@ -8,7 +8,7 @@ namespace VCenterMigrationTool.Helpers;
 /// Attached behavior to fix mouse wheel scrolling issues
 /// </summary>
 public static class ScrollBehavior
-{
+    {
     public static readonly DependencyProperty FixMouseWheelProperty =
         DependencyProperty.RegisterAttached(
             "FixMouseWheel",
@@ -17,42 +17,65 @@ public static class ScrollBehavior
             new PropertyMetadata(false, OnFixMouseWheelChanged));
 
     public static void SetFixMouseWheel (DependencyObject obj, bool value)
-    {
+        {
         obj.SetValue(FixMouseWheelProperty, value);
-    }
+        }
 
     public static bool GetFixMouseWheel (DependencyObject obj)
-    {
+        {
         return (bool)obj.GetValue(FixMouseWheelProperty);
-    }
+        }
 
     private static void OnFixMouseWheelChanged (DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        if (d is ScrollViewer scrollViewer)
         {
+        if (d is UIElement element)
+            {
             if ((bool)e.NewValue)
-            {
-                scrollViewer.PreviewMouseWheel += ScrollViewer_PreviewMouseWheel;
-            }
+                {
+                element.PreviewMouseWheel += Element_PreviewMouseWheel;
+                }
             else
-            {
-                scrollViewer.PreviewMouseWheel -= ScrollViewer_PreviewMouseWheel;
+                {
+                element.PreviewMouseWheel -= Element_PreviewMouseWheel;
+                }
             }
         }
-    }
 
-    private static void ScrollViewer_PreviewMouseWheel (object sender, MouseWheelEventArgs e)
-    {
-        if (sender is ScrollViewer scrollViewer)
+    private static void Element_PreviewMouseWheel (object sender, MouseWheelEventArgs e)
         {
-            // Calculate scroll amount (120 is the standard delta for one "notch" of scroll)
-            double scrollAmount = e.Delta / 120.0 * 50; // 50 pixels per scroll notch
+        if (sender is UIElement element)
+            {
+            // Find the ScrollViewer in the visual tree
+            var scrollViewer = FindScrollViewer(element);
 
-            // Scroll vertically
-            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - scrollAmount);
+            if (scrollViewer != null)
+                {
+                // Calculate scroll amount (120 is the standard delta for one "notch" of scroll)
+                double scrollAmount = e.Delta / 120.0 * 50; // 50 pixels per scroll notch
 
-            // Mark the event as handled
-            e.Handled = true;
+                // Scroll vertically
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - scrollAmount);
+
+                // Mark the event as handled
+                e.Handled = true;
+                }
+            }
+        }
+
+    private static ScrollViewer? FindScrollViewer (DependencyObject element)
+        {
+        if (element is ScrollViewer scrollViewer)
+            return scrollViewer;
+
+        // Search children
+        for (int i = 0; i < System.Windows.Media.VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+            var child = System.Windows.Media.VisualTreeHelper.GetChild(element, i);
+            var result = FindScrollViewer(child);
+            if (result != null)
+                return result;
+            }
+
+        return null;
         }
     }
-}
---
+++ Helpers/WindowScrollBehavior.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace VCenterMigrationTool.Helpers;
+
+/// <summary>
+/// Attached behavior to enable global mouse wheel scrolling at the window level
+/// </summary>
+public static class WindowScrollBehavior
+    {
+    public static readonly DependencyProperty EnableGlobalScrollProperty =
+        DependencyProperty.RegisterAttached(
+            "EnableGlobalScroll",
+            typeof(bool),
+            typeof(WindowScrollBehavior),
+            new PropertyMetadata(false, OnEnableGlobalScrollChanged));
+
+    public static void SetEnableGlobalScroll (DependencyObject obj, bool value)
+        {
+        obj.SetValue(EnableGlobalScrollProperty, value);
+        }
+
+    public static bool GetEnableGlobalScroll (DependencyObject obj)
+        {
+        return (bool)obj.GetValue(EnableGlobalScrollProperty);
+        }
+
+    private static void OnEnableGlobalScrollChanged (DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+        if (d is UIElement element)
+            {
+            if ((bool)e.NewValue)
+                {
+                element.PreviewMouseWheel += Element_PreviewMouseWheel;
+                }
+            else
+                {
+                element.PreviewMouseWheel -= Element_PreviewMouseWheel;
+                }
+            }
+        }
+
+    private static void Element_PreviewMouseWheel (object sender, MouseWheelEventArgs e)
+        {
+        // Find the ScrollViewer under the mouse cursor
+        var element = e.OriginalSource as DependencyObject;
+
+        // Traverse up the visual tree to find a ScrollViewer
+        while (element != null)
+            {
+            if (element is ScrollViewer scrollViewer && scrollViewer.IsVisible)
+                {
+                // Check if the ScrollViewer can actually scroll vertically
+                if (scrollViewer.ExtentHeight > scrollViewer.ViewportHeight)
+                    {
+                    double scrollAmount = e.Delta / 120.0 * 50; // 50 pixels per scroll notch
+                    double newOffset = scrollViewer.VerticalOffset - scrollAmount;
+
+                    // Clamp the offset to valid range
+                    newOffset = Math.Max(0, Math.Min(newOffset, scrollViewer.ScrollableHeight));
+
+                    scrollViewer.ScrollToVerticalOffset(newOffset);
+                    e.Handled = true;
+                    return;
+                    }
+                }
+
+            try
+                {
+                element = System.Windows.Media.VisualTreeHelper.GetParent(element);
+                }
+            catch
+                {
+                // Sometimes GetParent can throw exceptions, so we'll break out safely
+                break;
+                }
+            }
+        }
+    }
